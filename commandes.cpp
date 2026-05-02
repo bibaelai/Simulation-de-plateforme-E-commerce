c297cde94b4e1c5def2d830786b9c482f72b6d5f
FatimaEzzahraOulghazi <ezahrafatima392@gmail.com>
2026-05-02 20:42:03 +0100

    j'ai divise la classe le code de la classe commandes en commandes.h et commandes.cpp

--- commandes.cpp
+++ commandes.cpp
@@ -1,43 +1,9 @@
-#include <iostream>
-#include <string>
-using namespace std;
+#include "Commandes.h"
 
-enum EtatCommande { //enum remplace des chiffres incompréhensibles par des mots clairs.
-    EN_ATTENTE,
-    CONFIRMEE,
-    EN_LIVRAISON,
-    LIVREE,
-    ANNULEE
-};
-
-enum EtatCommande {
-    EN_ATTENTE,
-    CONFIRMEE,
-    EN_LIVRAISON,
-    LIVREE,
-    ANNULEE
-};
-
-class Commande {
-private:
-    // ===== ATTRIBUTS =====
-    int          id_commande;
-    string       date_commande;
-    EtatCommande etat;
-    Client*      client;  // pointeur vers le client qui a commandé
-    Produit**    produits;        // tableau dynamique ex: [*iPhone, *TV, *PC],pointeur vers tableau de pointeurs
-    int*         quantites;  // chaque quantites[i] correspond à produits[i]
-
-    int          nb_produits;    
-    Paiement*    paiement;
-    string       adresse_livraison;
-    double       frais_livraison;
-
-public:
     // ===== CONSTRUCTEUR / DESTRUCTEUR =====
 
     
-Commande(int id, Client* c, string date,string adresse, int nb) { 
+Commande::Commande(int id, Client* c, string date,string adresse, int nb) { 
     id_commande       = id;
     client            = c;
     date_commande     = date;
@@ -51,12 +17,12 @@ Commande(int id, Client* c, string date,string adresse, int nb) {
     produits  = new Produit*[nb_produits];
     quantites = new int[nb_produits];
 }
-~Commande() {
+Commande::~Commande() {
     delete[] produits;
     delete[] quantites;
 }
     // ===== METHODES =====
-void ajouterProduit(Produit* p, int quantite) {
+void Commande::ajouterProduit(Produit* p, int quantite) {
     for (int i = 0; i < nb_produits; i++) {
         if (produits[i] == NULL) {
             produits[i]  = p;
@@ -65,7 +31,7 @@ void ajouterProduit(Produit* p, int quantite) {
         }
     }
 }
-void supprimerProduit(int id_produit) {
+void Commande::supprimerProduit(int id_produit) {
     for (int i = 0; i < nb_produits; i++) {
         if (produits[i]->getId() == id_produit) {
             for (int j = i; j < nb_produits - 1; j++) {
@@ -79,21 +45,21 @@ void supprimerProduit(int id_produit) {
     }
     cout << "Produit non trouve." << endl;
 }
-double calculerTotal() {
+double commande::calculerTotal() {
     double total = 0;
     for (int i = 0; i < nb_produits; i++) {
         total += produits[i]->getPrix() * quantites[i];
     }
     return total;
 }
-double calculerTotalAvecLivraison() {
+double commande::calculerTotalAvecLivraison() {
     return calculerTotal() + frais_livraison;
 }
-void changerEtat(EtatCommande nouvelEtat) {
+void Commande::changerEtat(EtatCommande nouvelEtat) {
     etat = nouvelEtat;
 }
 
-void afficherCommande() {
+void Commande::afficherCommande() {
     cout << "===== Commande #" << id_commande << " =====" << endl;
     cout << "Date       : " << date_commande        << endl;
     cout << "Adresse    : " << adresse_livraison     << endl;
@@ -108,19 +74,19 @@ void afficherCommande() {
     cout << "Livraison  : " << frais_livraison             << " DH" << endl;
     cout << "TOTAL      : " << calculerTotalAvecLivraison()<< " DH" << endl;
 }
-bool estVide() {
+bool Commande::estVide() {
     return nb_produits == 0;
 }
 
     // ===== GETTERS =====
-int          getId()             { return id_commande; }
-string       getDate()           { return date_commande; }    EtatCommande getEtat()           { return etat; }
-int          getNbProduits()     { return nb_produits; }
-double       getFraisLivraison() { return frais_livraison; }
-string       getAdresse()        { return adresse_livraison; }
+int          Commande::getId() { return id_commande; }
+string       Commande::getDate() { return date_commande; }  
+EtatCommande Commande::getEtat() { return etat; }
+int          Commande::getNbProduits() { return nb_produits; }
+double       Commande::getFraisLivraison() { return frais_livraison; }
+string       Commande::getAdresse()        { return adresse_livraison; }
 
     // ===== SETTERS =====
-void setPaiement(Paiement* p)    { paiement = p; }
-void setAdresse(string adresse)  { adresse_livraison = adresse; }
-void setFraisLivraison(double f) { frais_livraison = f; }
-};
+void Commande::setPaiement(Paiement* p)    { paiement = p; }
+void commande::setAdresse(string adresse)  { adresse_livraison = adresse; }
+void Commande::setFraisLivraison(double f) { frais_livraison = f; }
